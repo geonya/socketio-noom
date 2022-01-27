c6d4825e9ca3cb549e358fd38feb7a85442e5a6b
Geony <geony8410@gmail.com>
2022-01-27 12:54:09 +0900

    make chat room and set nickname

--- src/public/app.js
+++ src/public/app.js
@@ -1,16 +1,62 @@
-import { CLOSING } from "ws";
-
 const socket = io();
+const join = document.getElementById("join");
+const joinForm = join.querySelector("form");
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-const handleRoomSubmit = (event) => {
+room.hidden = true;
+let roomName;
+let nickName;
+
+const addMessage = (message) => {
+	const ul = room.querySelector("ul");
+	const li = document.createElement("li");
+	li.innerText = message;
+	ul.appendChild(li);
+};
+const handleMessageSubmit = (event) => {
 	event.preventDefault();
-	const input = form.querySelector("input");
-	socket.emit("enter_room", { payload: input.value }, () => {
-		console.log("server is done"); // 서버에서 실행되는 콜백 함수
+	const input = room.querySelector("input");
+	const value = input.value;
+	socket.emit("new_message", input.value, roomName, () => {
+		addMessage(`You : ${value}`);
 	});
 	input.value = "";
 };
-form.addEventListener("submit", handleRoomSubmit);
+const showRoom = () => {
+	join.hidden = true;
+	room.hidden = false;
+	const h3 = room.querySelector("h3");
+	h3.innerText = `Hello ${nickName}, This is Room : ${roomName}`;
+	const messageForm = room.querySelector("form");
+	messageForm.addEventListener("submit", handleMessageSubmit);
+};
+
+const handleRoomSubmit = (event) => {
+	event.preventDefault();
+	const roomNameInput = document.querySelector("#join #roomname");
+	const nickNameInput = document.querySelector("#join #nickname");
+	socket.emit(
+		"enter_room",
+		roomNameInput.value,
+		nickNameInput.value,
+		showRoom
+	);
+	socket.emit("nickname", nickNameInput.value);
+	roomName = roomNameInput.value;
+	nickName = nickNameInput.value;
+};
+
+joinForm.addEventListener("submit", handleRoomSubmit);
+
+socket.on("join", (nickname) => {
+	addMessage(`${nickname} is arrived!`);
+});
+
+socket.on("bye", (nickname) => {
+	addMessage(`${nickname} left T^T`);
+});
+
+socket.on("new_message", (nickname, message) => {
+	addMessage(`${nickname} : ${message}`);
+});
--- src/server.js
+++ src/server.js
@@ -15,14 +15,26 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIo(httpServer);
 
 wsServer.on("connection", (socket) => {
-	socket.on("enter_room", (msg, done) => {
-		console.log(msg);
-		setTimeout(() => {
-			done();
-		}, 5000);
+	socket.onAny((event) => {
+		console.log(`Socket Event: ${event}`);
+	});
+	socket.on("enter_room", (room, nick, done) => {
+		socket.join(room);
+		done();
+		socket["nickname"] = nick || "anonymous";
+		socket.to(room).emit("join", socket.nickname); //send message everybody except me
+	});
+	socket.on("disconnecting", () => {
+		socket.rooms.forEach((room) =>
+			socket.to(room).emit("bye", socket.nickname)
+		);
+	});
+
+	socket.on("new_message", (message, room, done) => {
+		socket.to(room).emit("new_message", socket.nickname, message);
+		done();
 	});
 });
-
 httpServer.listen(process.env.PORT, () => {
 	console.log(`✅ Listening on http://localhost:${process.env.PORT} 🚀`);
 });
